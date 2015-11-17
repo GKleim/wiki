053714257a44f56c5f095a68f4f18252dc6726ac
GKleim <geoff.kleimeyer@gmail.com>
2015-11-16 21:45:41 -0600

    comment entities.py

--- imposing-cinema-111622/entities.py
+++ imposing-cinema-111622/entities.py
@@ -1,36 +1,21 @@
+# entities.py
+# This .py file contains datastore entities and related functions
+# This file should only contain datastore entitiy classes and relate memcache queries.
+# Improvement:
+#	(1) May want to organize memchache a bit better.
+
 from google.appengine.ext import db
 from google.appengine.api import memcache
 from datetime import datetime, timedelta
 from utils import make_pw_hash, valid_pw
 
-def top_entries(update = False):
-	key = 'top'
-	entries = memcache.get(key)
-	if entries is None or update:
-		print 'DB Query'
-		#database query for all posts
-		entries = Post.all().ancestor(blog_key()).order("-created").run(limit=10)
-		#db is not queried until something is ran on entries. Make a list out of the
-		#query to force the query to run. This prevents potentially running the query
-		#multiple times in later code
-		entries = list(entries)
-		memcache.set(key, entries)
-		memcache.set("time", datetime.now())
-	return entries
+# Post entity
+# The Post entity represents rows of blog posts (sometimes called entries)
 
-def get_entry(entry_id):
-	key = '%s' % entry_id
-	entry = memcache.get(key)
-	if entry is None:
-		#database query for post with entry_id
-		entry = Post.get_by_id(int(entry_id), parent=blog_key())
-		memcache.set(key, entry)
-		memcache.set('time|%s' % entry_id, datetime.now())
-	return entry
-
+# The blog_key function returns the parent key for posts.
+# Having a parent key guarantees consistency when querying immediately after creation (?)
 def blog_key(name = 'default'):
 	return db.Key.from_path('blogs', name)
-	# This is for adding a parent? I do not use this function in my code
 
 class Post(db.Model):
 	subject = db.StringProperty(required = True)
@@ -38,6 +23,7 @@ class Post(db.Model):
 	created = db.DateTimeProperty(auto_now_add = True)
 	modified = db.DateTimeProperty(auto_now = True)
 
+	# The as_dict function converts the Post object into a dictionary to be jsonified
 	def as_dict(self):
 		time_fmt = '%c'
 		d = {"subject": self.subject,
@@ -46,7 +32,52 @@ class Post(db.Model):
 	         "modified": self.modified.strftime(time_fmt)}
 		return d
 
+# The top_entries function returns the entries to be displayed on the homepage
+# This function uses memcache to ensure the query is run only when a new entry is added to the
+# datastore.
+# Improvements:
+#	(1) Add newly added entry to memcache without calling a query (imporves performance)
+def top_entries(update = False):
+	key = 'top'
+	entries = memcache.get(key)
+	if entries is None or update:
+		# print 'DB Query' when the query is run (for debugging)
+		print 'DB Query'
 
+		#database query for posts
+		entries = Post.all().ancestor(blog_key()).order("-created").run(limit=10)
+
+		#db is not queried until something is ran on entries. Make a list out of the
+		#query to force the query to run. This prevents potentially running the query
+		#multiple times in later code
+		entries = list(entries)
+
+		memcache.set(key, entries)
+
+		# "time" is stored for displaying the latest query on the webpage
+		memcache.set("time", datetime.now())
+	return entries
+
+# The get_entry function returns the requested entry
+# This function stores the entries in memcache when they are created
+def get_entry(entry_id):
+	key = '%s' % entry_id
+	entry = memcache.get(key)
+	if entry is None:
+		#database query for post with entry_id
+		entry = Post.get_by_id(int(entry_id), parent=blog_key())
+		memcache.set(key, entry)
+
+		# A "time" key is created for each entry for display on webpage
+		memcache.set('time|%s' % entry_id, datetime.now())
+	return entry
+
+
+# User entity
+# The User entity represents rows of user accounts
+
+# The users_key function returns the parent key for users.
+# Having a parent key guarantees consistency when querying immediately after creation (?)
 def users_key(group = 'default'):
 	return db.Key.from_path('users', group)
 
@@ -56,15 +87,20 @@ class User(db.Model):
 	email = db.StringProperty()
 	joined = db.DateTimeProperty(auto_now_add = True)
 
+	# NOTE: @classmethods are methods called on classes, not instances of classes
+
+	# The by_id classmethod returns the user object corresponding to the user id (uid)
 	@classmethod
 	def by_id(cls, uid):
 		return cls.get_by_id(uid, parent = users_key())
 
+	# The by_name classmethod returns the user object corresponding to the username
 	@classmethod
 	def by_name(cls, username):
 		u = cls.all().filter('username =', username).get()
 		return u
 
+	# The register classmethod creates a new User object and handles password hashing
 	@classmethod
 	def register(cls, username, password, email = None):
 		password = make_pw_hash(username, password)
@@ -73,6 +109,7 @@ class User(db.Model):
 					password = password,
 					email = email)
 
+	# The login classmethod signs the user into the website
 	@classmethod
 	def login(cls, username, password):
 		u = cls.by_name(username)
